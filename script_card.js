5cd5228fa312083516bf7c5ed6bb0403af560c18
YibinFeng98 <156506928+FengYibin66@users.noreply.github.com>
2024-01-15 13:50:09 +0800

    11

--- script_card.js
+++ script_card.js
@@ -3,11 +3,11 @@ document.querySelectorAll('.clickable-card').forEach(card => {
         document.getElementById('modalTitle').textContent = this.querySelector('.card-title').textContent;
         document.getElementById('modalImage').src = this.querySelector('img').src;
         document.getElementById('modalDescription').textContent = this.querySelector('.card-text').textContent;
+        document.getElementById('modalExtraDescription').textContent = this.querySelector('.card-extra-description').textContent;
         document.getElementById('enlargedCardModal').style.display = "block";
     });
 });
 
-// Close the modal when the 'x' is clicked
 document.querySelector('.close').addEventListener('click', function() {
     document.getElementById('enlargedCardModal').style.display = "none";
 });
